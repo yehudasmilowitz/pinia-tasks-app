08e0970ed050931b056598d1671ad0dd168e3ffc
Yehuda Smilowitz <yehudasmilowitz@gmail.com>
2024-07-14 19:12:06 -0400

    Add button to reset store. Extract properties from the store while keeping it's reactivity using storeToRefs().

--- src/App.vue
+++ src/App.vue
@@ -10,28 +10,28 @@
     <br />
 
     <nav class="filter">
-      <button @click="filter = 'all'">
-        All ({{ taskStore.tasks.length }})
-      </button>
+      <button @click="filter = 'all'">All ({{ tasks.length }})</button>
       <button @click="filter = 'favorites'">
-        Favorites ({{ taskStore.favorites.length }})
+        Favorites ({{ favorites.length }})
       </button>
     </nav>
 
-    <div v-if="taskStore.loading" class="loading">Loading tasks...</div>
+    <div v-if="loading" class="loading">Loading tasks...</div>
 
     <div v-if="filter === 'all'" class="task-list">
-      <p>All Tasks: {{ taskStore.tasks.length }}</p>
-      <div v-for="task in taskStore.tasks" :key="task.id">
+      <p>All Tasks: {{ tasks.length }}</p>
+      <div v-for="task in tasks" :key="task.id">
         <TaskDetails :task="task" :key="task.id" />
       </div>
     </div>
     <div v-if="filter === 'favorites'" class="task-list">
-      <p>Favorites: {{ taskStore.favorites.length }}</p>
-      <div v-for="task in taskStore.favorites" :key="task.id">
+      <p>Favorites: {{ favorites.length }}</p>
+      <div v-for="task in favorites" :key="task.id">
         <TaskDetails :task="task" :key="task.id" />
       </div>
     </div>
+
+    <button @click="taskStore.$reset()">Reset state</button>
   </main>
 </template>
 
@@ -41,6 +41,9 @@ import TaskDetails from "./components/TaskDetails.vue";
 import TaskForm from "./components/TaskForm.vue";
 const taskStore = useTaskStore();
 import { ref } from "vue";
+import { storeToRefs } from "pinia";
+
+const { tasks, favorites, loading } = storeToRefs(taskStore);
 
 taskStore.getTasks();
 
